225db969addab61f510b40d30c2dadc3f0fc6893
ming <eiyaming@163.com>
2019-10-21 10:43:11 +0800

    homework1

---
+++ homework1.py
@@ -0,0 +1,19 @@
+import translate
+OUTPUT = 'txt/homework1.txt'
+with open('txt/data.txt',"r") as f1:
+    str = f1.read()
+str = str.replace('.',' ')
+
+
+split = str.split('\n')
+with open(OUTPUT, "w", encoding='utf-8')as f:
+    f.write("translate begins!" + '\n')
+for i in range(0,len(split)-1):
+    # print(i)
+    aaa = split[i].replace('\n', ' ')+'.'
+    print(aaa)
+    if len(aaa)>2:
+        ch = translate.translate2(aaa[2:])
+        with open(OUTPUT, "a", encoding='utf-8')as f:
+            f.write(aaa+'\n')
+            f.write(' '+ch +'\n'+'\n')
